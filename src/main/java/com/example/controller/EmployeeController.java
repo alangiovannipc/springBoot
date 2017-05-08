d34b659da953bc90b478651c559b0b5d02657dd4
alangiovannipc <alangiovannipc@gmail.com>
2017-05-08 12:16:37 -0500

    Created service and restController

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+/**
+ * Created by alan on 5/8/17.
+ */
+
+import java.util.List;
+
+import com.example.model.Employee;
+import com.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public List<Employee> getEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public Employee getEmployee(@PathVariable("id") long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+}
